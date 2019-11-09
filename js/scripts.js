64f59ee8ae09171f237342695ef0bae90eb7daf9
Michael Wells <mwells1286@gmail.com>
2019-11-08 20:03:16 -0800

    add style to page, clean code, and add to readme.

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,22 @@ Customer = function(name, phone, address) {
   this.name = name,
   this.phone = phone,
   this.address = address
-}
+};
 PizzaTotal = function() {
-  this.pizzaPrice = [];
+  this.pizzaPrice = [],
   this.pizzaOrder = [],
   this.pizzaId = 0
 };
 PizzaTotal.prototype.addPizza = function(pizza, size, toppings, price) {
-  pizza.id = this.assignId(),
+  pizza.id = this.assignId();
   this.pizzaOrder.push(size, toppings, price);
-}
+};
 PizzaTotal.prototype.assignId = function() {
   this.pizzaId += 1;
   return this.pizzaId;
-}
+};
 PizzaTotal.prototype.orderTotal = function() {
-  let orderTotal = 0
+  let orderTotal = 0;
   for(i=0;i<this.pizzaPrice.length;i++) {
     orderTotal += this.pizzaPrice[i]
   };
@@ -26,16 +26,14 @@ PizzaTotal.prototype.orderTotal = function() {
 };
 Pizza = function(size, toppings) {
   this.size = size,
-  this.toppings = toppings
-  this.price = 0;
+  this.toppings = toppings,
+  this.price = 0
 };
 Pizza.prototype.priceCalculator = function() {
   toppingTotal = this.toppings.length;
   this.price = this.size + toppingTotal;
   pizzaTotal.pizzaPrice.push(this.price);
 };
-
-
 //UI
 Customer.prototype.submitOrder = function(customer) {
   var orderDetails = $('#delivery-details')
@@ -46,13 +44,11 @@ Customer.prototype.submitOrder = function(customer) {
     orderDetails.append('<li>' + customer[key] + '</li>')
   })
 }
-
 PizzaTotal.prototype.pizzaCartDisplay = function(pizza, pizzaTotal) {
-  $("#order-details").append("<li id=" + pizzaTotal.pizzaId + ">$"+pizza.price+"—"+pizza.size+" inch pizza with: cheese "+pizza.toppings.join(' ')+"</li>");
+  $("#order-details").append("<li id=" + pizzaTotal.pizzaId + ">$"+pizza.price+" | "+pizza.size+" inch pizza with: cheese "+pizza.toppings.join(' ')+"</li>");
   $('#cost').text("$" + pizzaTotal.orderTotal());
   $('.show-order').show();
 };
-
 let pizzaTotal = new PizzaTotal();
 $(document).ready(function() {
   $('form#build').submit(function(event) {
@@ -69,24 +65,17 @@ $(document).ready(function() {
     pizzaTotal.addPizza(size, toppings, price);
     pizzaTotal.pizzaCartDisplay(pizza, pizzaTotal);
   });
-
   $('#checkout').click(function(event) {
     event.preventDefault();
-  $('#submit-order').show();
-  $('#customer-order-input').hide();
-});
-
-
+    $('#submit-order').show();
+    $('#customer-order-input').hide();
+  });
   $('form#submit-order').submit(function(event) {
     event.preventDefault();
-
     let userName = $('#name-input').val();
     let userAddress = $('#address-input').val();
     let userPhone = $('#phone-input').val();
     let customer = new Customer(userName, userAddress, userPhone);
     customer.submitOrder(customer);
-
-    // $('#total-cost').text("$" + pizzaTotal.orderTotal());
-    // $('#customers-order').append(pizzaTotal.pizzaCartDisplay(pizza, pizzaTotal))
   });
 });
--- readme.md
+++ readme.md
@@ -1,26 +1,27 @@
-# _Name_ 
+# _Carnap's Pizza_
 
-#### _Short Description _
+#### _An online ordering application that allows users to build a pizza and submit it to the restaurant._
 
 #### By _**Michael Wells**_
 
 ## Description
-
-_ _
+This application will allow a user to select different toppings and sizes of a pizza, submit multiple orders, and add their address for delivery. There is a pizza building page, a submit order page, and a order confirmation page. The app can easily be modified to add more options or change other features.
 
 |Behavior|Input|Output|
 |---|---|---|
-|   |   |   |
-|   |   |   |
-|   |   |   |
+|  collects customer information | user enters delivery details  | app stores details as a customer object  |
+| customer selects pizza size and and toppings and it returns cost of pizza  | med, pepperoni, olives  | 15  |
+| user submits an order for multiple pizzas and it returns cost of all.  | User enters in information for three pizzas  | $38  |
+|  user submits order and receives confirmation with delivery details, order and total|  click submit | receives all details in order confirmation page  |
+
 
 ## Setup/Installation Requirements
 
-* _Launch <https://michael-wayne-wells.github.io/(---------)> in your browser._
+* _Launch <https://michael-wayne-wells.github.io/(pizza-shop)> in your browser._
 ### OR ###
 * clone and open the file locally
 * _in terminal run:_
->$git clone https://github.com/Michael-Wayne-Wells/(------).git
+>$git clone https://github.com/Michael-Wayne-Wells/pizza-shop.git
 * _open index.html in your browser._
 
 
